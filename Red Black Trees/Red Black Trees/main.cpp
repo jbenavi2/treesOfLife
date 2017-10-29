889f4cccaa291d6fa8495bea6e962f33a0362810
Javier <Javier@ECHOBLAST>
2017-10-29 15:33:07 -0700

    Initial class declaration

---
+++ Red Black Trees/Red Black Trees/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <stdio.h>
+#include <list>
+#include <iterator>
+#include <vector>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		printf("Error:  Include the name of the input file as an argument\n");
+		return 0;
+	}
+
+
+
+
+}
